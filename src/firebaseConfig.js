0a605b5984132f7e7759905317f10e7157408b6b
shwet <ppdshwet@gmail.com>
2026-02-17 16:00:54 +0530

    config of firebase done

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,39 +1,20 @@
-// 1. Import the necessary tools from the Firebase "library" we installed
+// firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
-/**
- * 🔹 TO THE DATABASE TEAM:
- * 1. Go to https://console.firebase.google.com/
- * 2. Click on your Project Name.
- * 3. Click the Gear Icon (Project Settings) > General.
- * 4. Scroll down to "Your Apps" and click the (</>) Web icon.
- * 5. Copy the 'firebaseConfig' object and paste the values below.
- */
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // The 'Key' that allows our website to talk to your Firebase project
-  apiKey: "PASTE_API_KEY_HERE",
-
-  // Your project's unique address on the internet
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-
-  // The unique ID of your Firebase project
-  projectId: "PASTE_PROJECT_ID_HERE",
-
-  // Where images/files will be stored (if we use it later)
-  storageBucket: "PASTE_PROJECT_ID.appspot.com",
-
-  // Used for sending notifications/messages
-  messagingSenderId: "PASTE_SENDER_ID_HERE",
-
-  // The unique ID for this specific Web App
-  appId: "PASTE_APP_ID_HERE"
+  apiKey: "AIzaSyB0yOUvC0m_IFYEISWaR1tKhA599EqtqMs",
+  authDomain: "project-management-syste-dba4b.firebaseapp.com",
+  projectId: "project-management-syste-dba4b",
+  storageBucket: "project-management-syste-dba4b.firebasestorage.app",
+  messagingSenderId: "80651715198",
+  appId: "1:80651715198:web:4b03ccbbf0eeb144e64e2f"
 };
 
-// 2. This line "turns on" the connection using the settings above
+// INITIALIZE APP FIRST
 const app = initializeApp(firebaseConfig);
 
-// 3. This line "turns on" the Authentication feature (the Login/Sign-up part)
-// Make sure you have enabled "Email/Password" in the Firebase Console under Build > Authentication
+// EXPORT SERVICES AFTER
 export const auth = getAuth(app);
+export const db = getFirestore(app);
